e48f97e70261b3fb9b5c992f97936b9423d9ebbd
OsiDrix <samuel_02100@hotmail.com>
2024-04-08 18:31:55 -0400

    Usuarios

--- MediaBookingAPI/Models/Usuario.cs
+++ MediaBookingAPI/Models/Usuario.cs
@@ -6,20 +6,21 @@ namespace MediaBookingAPI.Models
     {
 
         [Key]
-        public int Id_user { get; set; }
+        public int id_usuario { get; set; }
        
-        public string User {  get; set; }
-        public string Nombre_user { get; set; }
+        public string usuario {  get; set; }
+        public string nombre_usuario { get; set; }
 
-        public string Apellido_user { get; set; }
+        public string apellido_usuario { get; set; }
 
-        public string Correo_user { get; set; }
+        public string clave_usuario { get; set; }
 
-        public string Telefono_user { get; set; }
+        public string telefono_usuario { get; set; }
 
-        public string Direccion_user {  get; set; }
+        public string correo_usuario {  get; set; }
 
-        public string Contrasena_user { get; set; }
+        public string direccion_usuario { get; set; }
+        public string tipo_usuario { get; set; }
 
     }
 }
